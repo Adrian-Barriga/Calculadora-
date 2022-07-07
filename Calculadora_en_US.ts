4913f35681bf47051be61a7cc1ce3e68184f1b55
Adrian Barriga <abarrigac@est.ups.edu.ec>
2022-07-07 13:10:37 -0500

    Traducciones agregadas

--- Calculadora_en_US.ts
+++ Calculadora_en_US.ts
@@ -1,83 +1,83 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="Calculadora_es_EC">
+<TS version="2.1" language="en_US">
 <context>
     <name>Principal</name>
     <message>
         <location filename="principal.ui" line="14"/>
         <source>Principal</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Main</translation>
     </message>
     <message>
         <location filename="principal.ui" line="21"/>
         <source>Numero 1</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Number 1</translation>
     </message>
     <message>
         <location filename="principal.ui" line="31"/>
         <source>Numero 2</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Number 2</translation>
     </message>
     <message>
         <location filename="principal.ui" line="41"/>
         <source>Operacion</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Operation</translation>
     </message>
     <message>
         <location filename="principal.ui" line="49"/>
         <source>Sumar</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">addition</translation>
     </message>
     <message>
         <location filename="principal.ui" line="54"/>
         <source>Multiplicar</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">multiply</translation>
     </message>
     <message>
         <location filename="principal.ui" line="59"/>
         <source>Restar</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Subtract</translation>
     </message>
     <message>
         <location filename="principal.ui" line="64"/>
         <source>Dividir</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Split</translation>
     </message>
     <message>
         <location filename="principal.ui" line="85"/>
         <location filename="principal.ui" line="111"/>
         <source>Calcular</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Calculate</translation>
     </message>
     <message>
         <location filename="principal.ui" line="102"/>
         <source>Archivo</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">FIle</translation>
     </message>
     <message>
         <location filename="principal.ui" line="114"/>
         <source>F1</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">F1</translation>
     </message>
     <message>
         <location filename="principal.ui" line="119"/>
         <source>Salir</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Quit</translation>
     </message>
     <message>
         <location filename="principal.ui" line="122"/>
         <source>Ctrl+Q</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Ctrl+Q</translation>
     </message>
     <message>
         <location filename="principal.cpp" line="54"/>
         <source>Calculo</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Calculus</translation>
     </message>
     <message>
         <location filename="principal.cpp" line="55"/>
         <source>Elresultado es: %1</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">The result is</translation>
     </message>
 </context>
 </TS>
--- Calculadora_fr.ts
+++ Calculadora_fr.ts
@@ -1,83 +1,83 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="Calculadora_es_EC">
+<TS version="2.1" language="fr_FR">
 <context>
     <name>Principal</name>
     <message>
         <location filename="principal.ui" line="14"/>
         <source>Principal</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Principal</translation>
     </message>
     <message>
         <location filename="principal.ui" line="21"/>
         <source>Numero 1</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Numéro 1</translation>
     </message>
     <message>
         <location filename="principal.ui" line="31"/>
         <source>Numero 2</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Numéro 2</translation>
     </message>
     <message>
         <location filename="principal.ui" line="41"/>
         <source>Operacion</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Opération</translation>
     </message>
     <message>
         <location filename="principal.ui" line="49"/>
         <source>Sumar</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Ajouter</translation>
     </message>
     <message>
         <location filename="principal.ui" line="54"/>
         <source>Multiplicar</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Multiplier</translation>
     </message>
     <message>
         <location filename="principal.ui" line="59"/>
         <source>Restar</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Soustraire</translation>
     </message>
     <message>
         <location filename="principal.ui" line="64"/>
         <source>Dividir</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Diviser</translation>
     </message>
     <message>
         <location filename="principal.ui" line="85"/>
         <location filename="principal.ui" line="111"/>
         <source>Calcular</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Calculer</translation>
     </message>
     <message>
         <location filename="principal.ui" line="102"/>
         <source>Archivo</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Fichier</translation>
     </message>
     <message>
         <location filename="principal.ui" line="114"/>
         <source>F1</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">F1</translation>
     </message>
     <message>
         <location filename="principal.ui" line="119"/>
         <source>Salir</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">sortir</translation>
     </message>
     <message>
         <location filename="principal.ui" line="122"/>
         <source>Ctrl+Q</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Ctrl+Q</translation>
     </message>
     <message>
         <location filename="principal.cpp" line="54"/>
         <source>Calculo</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Calcul</translation>
     </message>
     <message>
         <location filename="principal.cpp" line="55"/>
         <source>Elresultado es: %1</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Le résultat est</translation>
     </message>
 </context>
 </TS>
